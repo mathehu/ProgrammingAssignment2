8ec6f638e58b2742bcfeb5ddaef872cd511757dd
Markus Themessl <90453920+mathehu@users.noreply.github.com>
2021-12-10 15:02:45 +0100

    2nd attempt
    I realised that my R script had not been uploaded during my first attempt. Hopefully, the second time works. This assignment was very challenging for me. I oriented myself closely on the solution posted by Guangming Lang on https://masterr.org/r/how-to-cache-a-matrix-inversion-in-r/

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+         ##Set the vector's value
+   m <- NULL
+  set <- function(y) {
+    x <<- y   ##the double arrow assigns a value from a different environment
+    m <<- NULL
+  }
+  ##get the vector's value
+    get <- function() X
+  ##set the inverse's value
+    setInverse <- function(inverse) m <<- inverse
+  ##get the inverse's value
+     getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +26,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ##checks to see if the inverse has already been calculated
+  m <- x$getInverse()
+  ##if inverse is calculated, gets it from cache and skips computation
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+ ##calculates inverse of the matrix
+   mat <- x$get()
+  m <- solve(mat,...)
+ ##sets the inversed values in the cache
+   x$setInverse(m)
+
+##Prints the matrix to the console
+  return(m)
 }
+
